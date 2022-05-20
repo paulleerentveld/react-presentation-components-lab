c9898d0efb6d59404b677324fefc76c56276cb9b
paulleerentveld <paul@veldnet.com.au>
2022-05-20 16:41:22 +1000

    lab done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={e => console.log("I worked")} />
   </div>,
   document.getElementById('root')
 );
